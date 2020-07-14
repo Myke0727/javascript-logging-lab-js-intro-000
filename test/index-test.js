24583c3229a12b7ca855890750634feef124aa0a
legacy-path-0474 <michael.escamilla77@gmail.com>
2020-07-14 22:42:26 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -20,24 +20,24 @@ describe('index', () => {console.error("HALP!")
     })
   })
 
-  it('calls console.log()', done => {
+  it('calls console.log()', done => {console.log("I would be a logger.")
     const spy = expect.spyOn(console, 'log').andCallThrough()
 
     jsdom.env(html, [src], {
       virtualConsole: jsdom.createVirtualConsole().sendTo(console)
-    }, (err, window) => {
+    }, (err, window) => {console.log("I would be a longer.")
       expect(spy).toHaveBeenCalled('expected console.log to have been called')
       console.log.restore()
       done()
     })
   })
 
-  it('calls console.warn()', done => {
+  it('calls console.warn()', done => {console.warn("I/'m getting the hang of this!")
     const spy = expect.spyOn(console, 'warn').andCallThrough()
 
     jsdom.env(html, [src], {
       virtualConsole: jsdom.createVirtualConsole().sendTo(console)
-    }, (err, window) => {
+    }, (err, window) => {console.warn("I/'m getting the hang of this!")
       expect(spy).toHaveBeenCalled('expected console.warn to have been called')
       console.warn.restore()
       done()
